6f6fdb9d6fd4552170ca35d1cb0ee3b69ba50a88
vloooo <vlad.m.o12@gmail.com>
2024-02-08 18:23:44 +0200

    comment

---
+++ tests/unit/test_first.py
@@ -0,0 +1,20 @@
+from first import add_numbers, factorial, is_even_or_odd
+
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
+
+
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(5) == 120
+    assert factorial(10) == 3628800
+
+def test_is_even_or_odd():
+    assert is_even_or_odd(4) == "Evenl"
+    assert is_even_or_odd(7) == "Odd"
+    assert is_even_or_odd(0) == "Even"
+    assert is_even_or_odd(-3) == "Odd"
